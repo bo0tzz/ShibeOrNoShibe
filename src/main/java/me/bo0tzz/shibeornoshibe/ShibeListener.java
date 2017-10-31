6358b60cce1edaaf086a86772f9d3871901ec2af
Bo0tzz <btdewilligen@gmail.com>
2017-10-31 23:28:47 +0100

    Add caching

--- src/main/java/me/bo0tzz/shibeornoshibe/ShibeListener.java
+++ src/main/java/me/bo0tzz/shibeornoshibe/ShibeListener.java
@@ -7,19 +7,33 @@ import pro.zackpollard.telegrambot.api.event.chat.message.PhotoMessageReceivedEv
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 public class ShibeListener implements Listener {
     private final ShibeOrNoShibe main;
     private final ShibeTester shibeTester;
+    private final Map<String, ShibeResult> cachedResults;
     private final static String OUTPUT = "*Shiba*: %.2f%%\n*Doggo:* %.2f%%\n*Random*: %.2f%%";
 
     public ShibeListener(ShibeOrNoShibe main) {
         this.main = main;
         this.shibeTester = new ShibeTester();
+        this.cachedResults = new HashMap<>();
     }
 
     public void onPhotoMessageReceived(PhotoMessageReceivedEvent event) {
+        ShibeResult cached = cachedResults.get(event.getContent().getContent()[0].getFileId());
+        if (cached != null) {
+            HashMap<String, Float> prediction = cached.getPrediction();
+            String out = String.format(OUTPUT,
+                    prediction.get("shiba")*100,
+                    prediction.get("doggo")*100,
+                    prediction.get("random")*100);
+            event.getChat().sendMessage(SendableTextMessage.markdown(out).build());
+            return;
+        }
+
         File image;
         try {
             image = File.createTempFile(UUID.randomUUID().toString(), ".tmp");
@@ -37,6 +51,9 @@ public class ShibeListener implements Listener {
             event.getChat().sendMessage("Something has gone wrong while checking if your image is a shibe! If this keeps happening, please contact @bo0tzz");
             return;
         }
+
+        cachedResults.put(event.getContent().getContent()[0].getFileId(), confidence);
+
         HashMap<String, Float> prediction = confidence.getPrediction();
         String out = String.format(OUTPUT,
                 prediction.get("shiba")*100,
