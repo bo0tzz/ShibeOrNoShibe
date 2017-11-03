519707a7e83f99ec2c630b9eb0c44486ed218ea3
Bo0tzz <btdewilligen@gmail.com>
2017-11-03 13:41:04 +0100

    Add commands

--- src/main/java/me/bo0tzz/shibeornoshibe/engine/ShibeCommandListener.java
+++ src/main/java/me/bo0tzz/shibeornoshibe/engine/ShibeCommandListener.java
@@ -1,10 +1,12 @@
 package me.bo0tzz.shibeornoshibe.engine;
 
 import me.bo0tzz.shibeornoshibe.ShibeOrNoShibe;
+import me.bo0tzz.shibeornoshibe.bean.ShibeUser;
 import me.bo0tzz.shibeornoshibe.db.ShibeMorphia;
 import pro.zackpollard.telegrambot.api.chat.message.send.SendableTextMessage;
 import pro.zackpollard.telegrambot.api.event.Listener;
 import pro.zackpollard.telegrambot.api.event.chat.message.CommandMessageReceivedEvent;
+import pro.zackpollard.telegrambot.api.user.User;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -36,17 +38,35 @@ public class ShibeCommandListener implements Listener {
         event.getChat().sendMessage(SendableTextMessage.markdown(
            "*Welcome to the Shibe or no Shibe bot!*\n" +
                    "This bot uses a neural net to determine the content of images you send.\n" +
-                   "You can tell the bot to tag you when an image of a shibe or a doggo is sent, using the respective commands.\n" +
-                   "If you have any questions, problems or feature requests, please do not hesitate to contact @bo0tzz!" +
+                   "You can tell the bot to tag you when an image of a shibe or a doggo is sent, using the respective commands /shibe and /doggo.\n" +
+                   "If you have any questions, problems or feature requests, please do not hesitate to contact @bo0tzz!\n" +
                    "The source of this bot is available on [GitHub](https://github.com/bo0tzz/ShibeOrNoShibe)."
         ).build());
     }
 
     private void shibe(CommandMessageReceivedEvent event) {
-
+        User user = event.getMessage().getSender();
+        ShibeUser shibeUser = morphia.getShibeUser(user.getId());
+        boolean oldSetting = shibeUser.isPingShibe();
+        shibeUser = shibeUser.setPingShibe(!oldSetting);
+        boolean success = morphia.updateUser(shibeUser);
+        if (!success) morphia.saveShibeUser(shibeUser);
+        if (oldSetting)
+            event.getChat().sendMessage("You are no longer subscribed to receive mentions when a shibe image is sent!");
+        else
+            event.getChat().sendMessage("You are now subscribed to receive mentions when a shibe image is sent!");
     }
 
     private void doggo(CommandMessageReceivedEvent event) {
-
+        User user = event.getMessage().getSender();
+        ShibeUser shibeUser = morphia.getShibeUser(user.getId());
+        boolean oldSetting = shibeUser.isPingDoggo();
+        shibeUser = shibeUser.setPingDoggo(!oldSetting);
+        boolean success = morphia.updateUser(shibeUser);
+        if (!success) morphia.saveShibeUser(shibeUser);
+        if (oldSetting)
+            event.getChat().sendMessage("You are no longer subscribed to receive mentions when a doggo image is sent!");
+        else
+            event.getChat().sendMessage("You are now subscribed to receive mentions when a doggo image is sent!");
     }
 }
